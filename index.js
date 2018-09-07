5e9b2c2e502e8af2e31a322e908c6655ffb250df
María Fernanda Benítez Rivera <39814132+FeerRivera@users.noreply.github.com>
2018-09-07 18:59:53 -0500

    funciones

--- index.js
+++ index.js
@@ -1,23 +1,80 @@
-var fs = require('fs')
+let fs = require('fs');
+let path = require('path');
+const marked = require('marked');
+const fetch = require('node-fetch');
+const colors = require('colors');
 
-const readReadme = () => {
-  fs.readFile('./README.md', 'utf8', (err,data) => {
-    if(err){
-    console.log(err)
-  } else{
-   link(data);
-  }
+const mdLinks = function markdownLinkExtractor(markdown) {
+  
+  const links = [];
+  const renderer = new Marked.Renderer();
+  const linkWithImageSizeSupport = /^!?\[((?:\[[^\[\]]*\]|\\[\[\]]?|`[^`]*`|[^\[\]\\])*?)\]\(\s*(<(?:\\[<>]?|[^\s<>\\])*>|(?:\\[()]?|\([^\s\x00-\x1f()\\]*\)|[^\s\x00-\x1f()\\])*?(?:\s+=(?:[\w%]+)?x(?:[\w%]+)?)?)(?:\s+("(?:\\"?|[^"\\])*"|'(?:\\'?|[^'\\])*'|\((?:\\\)?|[^)\\])*\)))?\s*\)/;
 
-  })
-}
-readReadme();
+  Marked.InlineLexer.rules.normal.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.gfm.link = linkWithImageSizeSupport;
+  Marked.InlineLexer.rules.breaks.link = linkWithImageSizeSupport;
 
-const link = (data) => {
-   let regExp = /((http:\/\/|https:\/\/|www\.)[^\s]+)/gim;
-   let links = data.match(regExp);
+  renderer.link = (href, title, text) => {
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
 
-   if(links){
-     console.log(links)
-}
-
+  renderer.image = (href, title, text) => {
+   
+    href = href.replace(/ =\d*%?x\d*%?$/, '');
+    links.push({
+      href: href,
+      text: text,
+      title: title,
+    });
+  };
+  Marked(markdown, {renderer: renderer});
+  return links;
 };
+
+let currentDirectory = process.cwd(); 
+console.log(`Directorio actual: ${process.cwd()}`);
+let cwdBuffer = Buffer.from(currentDirectory);
+const [,, ...userArgs] = process.argv; 
+let validate = userArgs[0];
+
+fs.readdir(cwdBuffer, (err, files) => { 
+  if (err) {
+    console.log(err.message);
+  } else {
+    files.forEach(file => {
+      if (path.extname(file) === '.md') { 
+        fs.readFile(file, 'utf8', function(err, data) { 
+          if (err) { 
+            console.log(err);
+          } else {
+            mdLinks(data).forEach(element => {
+              if (validate === '--validate') {
+                fetch(`${element.href}`).then((response) => {
+                  
+                  console.log(`Archivo: ${file.blue} Texto: ${element.text.green} Link: ${response.url.yellow} Status: ${response.status} ${response.statusText.cyan}`);
+                }).catch((err) => {
+                 
+                  console.error(`Enlace roto --> Archivo: ${file.blue} Texto: ${element.text.green} Link: ${element.href.yellow}` + err);
+                });
+              } else {
+                fetch(`${element.href}`).then((response) => {
+                  
+                  console.log(`Archivo: ${file.blue} Texto: ${element.text.green} Link: ${element.href.yellow}`);
+                }).catch((err) => {
+                  
+                  console.error(`Enlace roto --> Archivo: ${file.blue} Texto: ${element.text.green} Link: ${element.href.yellow}` + err);
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
+
+module.exports = mdLinks;
